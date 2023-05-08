a94e388a52da22e28172a77036bf0485d4c3355e
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-05-07 22:24:03 -0300

    TDD que permita crear un tragamoneda

--- test/ar/edu/unlam/pb2/practicaTDDtragamonedas/TestTragamonedas.java
+++ test/ar/edu/unlam/pb2/practicaTDDtragamonedas/TestTragamonedas.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ar.edu.unlam.pb2.Tragamonedas.Tambores;
+import ar.edu.unlam.pb2.Tragamonedas.Tragamonedas;
 
 public class TestTragamonedas {
 
@@ -30,5 +31,18 @@ public class TestTragamonedas {
 		assertTrue("El valor esta entre 1 y 3", valorObtenido>=minimo && valorObtenido<=maximo);
 		
 	}
+	
+	@Test
+	public void testQuePermitaCrearUnTragamoneda() {
+		Tambores tambor1=new Tambores(6);
+		Tambores tambor2=new Tambores(6);
+		Tambores tambor3=new Tambores(6);
+		
+		Tragamonedas tragamoneda=new Tragamonedas(tambor1,tambor2,tambor3);
+		
+		assertNotNull(tragamoneda);
+		
+	}
+
 
 }
