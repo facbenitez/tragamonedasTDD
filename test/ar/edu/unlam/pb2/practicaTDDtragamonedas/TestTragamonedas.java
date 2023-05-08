4ab7dc1ef8889b1d2658d9db729b81b32f455dd9
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-05-07 22:20:56 -0300

    TDD que tambor cree valor con cantidad de caras

--- test/ar/edu/unlam/pb2/practicaTDDtragamonedas/TestTragamonedas.java
+++ test/ar/edu/unlam/pb2/practicaTDDtragamonedas/TestTragamonedas.java
@@ -16,5 +16,19 @@ public class TestTragamonedas {
 		assertNotNull(tambor1);
 		
 	}
+	
+	@Test      
+	public void testQueTamborCreeValorConCantidadDeCaras() {
+		Integer caras=3;
+		Tambores tambor1=new Tambores(caras);
+		Integer minimo=1;
+		Integer maximo=3;
+		
+		tambor1.girar();
+		Integer valorObtenido=tambor1.mostrarValor();
+		
+		assertTrue("El valor esta entre 1 y 3", valorObtenido>=minimo && valorObtenido<=maximo);
+		
+	}
 
 }
